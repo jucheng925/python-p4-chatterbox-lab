345530963f9496881d061a717360728f902abbb9
Julie Cheng <jucheng925@gmail.com>
2023-12-19 12:15:47 -0500

    first full stack app

--- server/app.py
+++ server/app.py
@@ -14,13 +14,48 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=["GET", "POST"])
 def messages():
-    return ''
+    if request.method == "GET":
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        messages_dict = [message.to_dict() for message in messages]
+        return make_response(messages_dict, 200)
+    elif request.method == "POST":
+        #get_json() - convert the json to a list/dict
+        json_data = request.get_json()
+        new_message = Message(
+            username=json_data["username"],
+            body=json_data["body"],
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        return make_response(new_message.to_dict(), 201)
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=["GET","PATCH", "DELETE"])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+    if message == None:
+        body = {"message": "Message id not found. Please try again"}
+        return make_response(body, 404)
+    else:
+        if request.method == "GET":
+            return make_response(message.to_dict(), 200)
+        elif request.method == "PATCH":
+            json_data = request.get_json()
+            for attr in json_data:
+                setattr(message, attr, json_data.get(attr))
+            db.session.add(message)
+            db.session.commit()
+            return make_response(message.to_dict(), 200)
+        elif request.method == "DELETE":
+            db.session.delete(message)
+            db.session.commit()
+            body = {
+                "delete_successful": True,
+                "message": "Message deleted"
+            }
+            return make_response(body, 200)
+
 
 if __name__ == '__main__':
     app.run(port=5555)
